3ba79236669da6f1c45cab4b7b1cc275eb2d2079
linkstuy <90122236+linkstuy@users.noreply.github.com>
2021-09-06 01:22:18 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="go-download.html"}
 if(x=="2"){xxx="link.html"}
 if(x=="3"){xxx="download.html"}
-if(x=="4"){xxx="Go.html"}a_to_ve[i].href="https://linkstuy.blogspot.com/2021/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="Go.html"}a_to_ve[i].href="https://dl.linkfirmware.com/2021/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
